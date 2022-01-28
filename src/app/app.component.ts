fe934071bd5cc544752253aadaba79274ba2113f
Cconkers <rubenpv011992@gmail.com>
2022-01-28 11:28:12 +0100

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './post.service';
+import { Post } from './post';
+import { Test } from './test';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-new-app';
+export class AppComponent implements OnInit {
+  name = 'Angular';
+
+  posts1 = new Array<Post>();
+  posts = new Array<Test["posts"]>();
+  comments = new Array<Test["comments"]>();
+  profile = new Array<Test["profile"]>();
+
+  constructor(private service: PostService) { }
+
+  ngOnInit() {
+    // this.service.getPosts().subscribe(response => {
+    //   this.posts = response.map(item => {
+    //     return new Post(
+    //       item.body,
+    //       item.id,
+    //       item.title,
+    //       item.userId
+    //     );
+    //   });
+    // });
+
+    this.service.getTests().subscribe(o =>
+      this.posts = o.map((item: Test["posts"]) => {
+        return new Test["posts"](
+          [{item}]
+        )
+      })
+    )
+
+
+
+
+
+  }
 }
+
